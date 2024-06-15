2fe768c0c5b14f3b4cc68e5fc343868e062ea3d1
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2024-06-15 18:30:36 +0100

    added routing and home page

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/home'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div>hello world</div>
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
