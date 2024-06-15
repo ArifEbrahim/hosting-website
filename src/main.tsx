efad3dd157dd2abc302a7436a2414a6ee2d9b0db
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2024-06-15 18:59:47 +0100

    added basic text and styling to home page

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
+import './main.css'
 
 const router = createBrowserRouter([
   {
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import styles from './Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <section className={styles['product-overview']}>
+        <h1>Get the freedom you deserve.</h1>
+      </section>
+      <section>
+        <h1 className={styles['section-title']}>Choose Your Plan</h1>
+        <p>Make sure you get the most for your money!</p>
+      </section>
+    </>
+  )
+}
