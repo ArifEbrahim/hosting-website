1d1a52caef77c1d12b649c3b9588f49fab0efe0e
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2024-06-17 20:26:04 +0100

    wokring nav and routes

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,24 +1,27 @@
-import { Link } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 
 export default function Nav() {
   return (
-    <header>
-      <div>
-        <a href="index.html">uHost</a>
-      </div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/packages">Packages</Link>
-          </li>
-          <li>
-            <Link to="/customers">Customers</Link>
-          </li>
-          <li>
-            <Link to="/start-hosting">Start hosting</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header>
+        <div>
+          <a href="index.html">uHost</a>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/packages">Packages</Link>
+            </li>
+            <li>
+              <Link to="/customers">Customers</Link>
+            </li>
+            <li>
+              <Link to="/hosting">Start hosting</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <Outlet />
+    </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home'
+import Packages from './pages/Packages'
+import Customers from './pages/Customers'
+import Hosting from './pages/Hosting'
+import Nav from './components/nav'
 import './main.css'
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Home />
+    element: <Nav />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/packages',
+        element: <Packages />
+      },
+      {
+        path: '/customers',
+        element: <Customers />
+      },
+      {
+        path: '/hosting',
+        element: <Hosting />
+      }
+    ]
   }
 ])
 
