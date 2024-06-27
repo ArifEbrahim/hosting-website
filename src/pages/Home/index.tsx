89438cf9fc0b8917f917113f0f18f3aee823c67a
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2024-06-27 21:06:25 +0100

    choose plan section complete

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,7 +8,53 @@ export default function Home() {
       </section>
       <section>
         <h1 className={styles['section-title']}>Choose Your Plan</h1>
-        <p>Make sure you get the most for your money!</p>
+        <div>
+          <article className={styles['plan']}>
+            <h1 className={styles['plan__title']}>FREE</h1>
+            <h2 className={styles['plan__price']}>£0/month</h2>
+            <h3>For hobby projects or small teams.</h3>
+            <ul className={styles['plan__features']}>
+              <li>1 Workspace</li>
+              <li>Unlimited Traffic</li>
+              <li>10GB Storage</li>
+              <li>Basic Support</li>
+            </ul>
+            <div>
+              <button className={styles['button']}>CHOOSE PLAN</button>
+            </div>
+          </article>
+          <article
+            className={`${styles['plan']} ${styles['plan--highlighted']}`}
+          >
+            <h1 className={styles['plan__annotation']}>RECOMMENDED</h1>
+            <h1 className={styles['plan__title']}>PLUS</h1>
+            <h2 className={styles['plan__price']}>£29/month</h2>
+            <h3>For ambitious projects.</h3>
+            <ul className={styles['plan__features']}>
+              <li>5 Workspaces</li>
+              <li>Unlimited Traffic</li>
+              <li>100GB Storage</li>
+              <li>Plus Support</li>
+            </ul>
+            <div>
+            <button className={styles['button']}>CHOOSE PLAN</button>
+            </div>
+          </article>
+          <article className={styles['plan']}>
+            <h1 className={styles['plan__title']}>PREMIUM</h1>
+            <h2 className={styles['plan__price']}>£99/month</h2>
+            <h3>Your enterprise solution.</h3>
+            <ul className={styles['plan__features']}>
+              <li>10 Workspaces</li>
+              <li>Unlimited Traffic</li>
+              <li>Unlimited Storage</li>
+              <li>Priority Support</li>
+            </ul>
+            <div>
+            <button className={styles['button']}>CHOOSE PLAN</button>
+            </div>
+          </article>
+        </div>
       </section>
     </main>
   )
