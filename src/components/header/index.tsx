a6d0771d903199415ea5385ca3b1bc91a2336763
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2024-08-12 09:00:21 +0100

    update styles

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,22 +3,22 @@ import styles from './Header.module.css'
 
 export default function Header() {
   return (
-    <header className={styles['main-header']}>
+    <header className={styles['main_header']}>
       <div>
-        <NavLink to="/" className={styles['main-header__brand']}>
+        <NavLink to="/" className={styles['main_header__brand']}>
           uHost
         </NavLink>
       </div>
-      <nav className={styles['main-nav']}>
-        <ul className={styles['main-nav__items']}>
-          <li className={styles['main-nav__item']}>
+      <nav className={styles['main_nav']}>
+        <ul className={styles['main_nav__items']}>
+          <li className={styles['main_nav__item']}>
             <NavLink to="/packages">Packages</NavLink>
           </li>
-          <li className={styles['main-nav__item']}>
+          <li className={styles['main_nav__item']}>
             <NavLink to="/customers">Customers</NavLink>
           </li>
           <li
-            className={`${styles['main-nav__item']} ${styles['main-nav__item--cta']}`}
+            className={`${styles['main_nav__item']} ${styles['main_nav__item__cta']}`}
           >
             <NavLink to="/hosting">Start hosting</NavLink>
           </li>
